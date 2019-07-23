040a4114ad5028c9cbdb350025495b998a471f4c
Simone Vittori <hello@simonewebdesign.it>
2019-07-23 15:15:44 +0100

    Control

--- missions/control.lua
+++ missions/control.lua
@@ -3,7 +3,7 @@ function test_if_then()
   if true then
     result = 'true value'
   end
-  assert_equal(__, result)
+  assert_equal('true value', result)
 end
 
 function test_if_then_else()
@@ -13,7 +13,7 @@ function test_if_then_else()
   else
     result = 'false value'
   end
-  assert_equal(__, result)
+  assert_equal('true value', result)
 end
 
 function test_if_then_elseif_else()
@@ -25,7 +25,7 @@ function test_if_then_elseif_else()
   else
     result = 'default value'
   end
-  assert_equal(__, result)
+  assert_equal('second value', result)
 end
 
 function test_if_not_statement()
@@ -33,21 +33,31 @@ function test_if_not_statement()
   if not false then
     result = 'updated value'
   end
-  assert_equal(__, result)
+  assert_equal('updated value', result)
 end
 
 function test_and_or_expression()
-  assert_equal( __,  (true and 'true value' or 'false value') )
-  assert_equal( __, (false and 'true value' or 'false value') )
+  assert_equal( 'true value',  (true and 'true value' or 'false value') )
+  assert_equal( 'false value', (false and 'true value' or 'false value') )
 end
 
+-- 1 * 1 = 1
+-- 1 * 2 = 2
+-- 2 * 3 = 6
+-- 6 * 4 = 24
+-- 24 * 5 = 120
+-- 120 * 6 = 720
+-- 720 * 7 = 5040
+-- 5040 * 8 = 40320
+-- 40320 * 9 = 362880
+-- 362880 * 10 = 3628800
 function test_while()
   local i, result = 1,1
   while i <= 10 do
     result = result * i
     i = i + 1
   end
-  assert_equal(__, result)
+  assert_equal(3628800, result)
 end
 
 function test_break()
@@ -57,7 +67,7 @@ function test_break()
     result = result * i
     i = i + 1
   end
-  assert_equal(__, result)
+  assert_equal(3628800, result)
 end
 
 function test_repeat()
@@ -66,7 +76,7 @@ function test_repeat()
     result = result * i
     i = i + 1
   until i==11
-  assert_equal(__, result)
+  assert_equal(3628800, result)
 end
 
 function test_numeric_for_creates_a_local_variable_not_available_outside_the_loop()
@@ -75,8 +85,8 @@ function test_numeric_for_creates_a_local_variable_not_available_outside_the_loo
   for i=1, #t do
     table.insert(result, t[i])
   end
-  assert_equal(__, table.concat(result, ' '))
-  assert_equal(__, i)
+  assert_equal('fish and chips', table.concat(result, ' '))
+  assert_equal(nil, i)
 end
 
 function test_numeric_for_with_step()
@@ -85,16 +95,16 @@ function test_numeric_for_with_step()
   for i=2, #t, 2 do -- notice the two here
     table.insert(result, t[i])
   end
-  assert_equal(__, table.concat(result, ', '))
+  assert_equal('2, 4, 6', table.concat(result, ', '))
 end
 
 function test_generic_for_over_array_like_tables_using_ipairs()
-  local t = { 'fish', 'and', 'chips' }
-  local result = {}
+   t = { 'fish', 'and', 'chips' }
+   result = {}
   for key,value in ipairs(t) do
     table.insert(result, value)
   end
-  assert_equal(__, table.concat(result, ' '))
+  assert_equal('fish and chips', table.concat(result, ' '))
 end
 
 function test_generic_for_over_hash_like_tables_using_pairs()
@@ -103,6 +113,6 @@ function test_generic_for_over_hash_like_tables_using_pairs()
   for k,v in pairs(t) do
     result[k] = v
   end
-  assert_equal(__, result.a)
-  assert_equal(__, result.b)
+  assert_equal(1, result.a)
+  assert_equal(2, result.b)
 end
